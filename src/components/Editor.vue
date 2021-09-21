a7226f2bf145070dfea97f45562b0ea6c5b223d6
beckho <vngkgk624@gmail.com>
2021-09-21 12:54:01 +0900

    flutter webview 와 통신 부분 추가테스트

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,8 +2,14 @@
   <div class="hello">
     <div id="editor"></div>
     console 에
+    <v-text-field v-model="testData" @change="onchange">
+
+    </v-text-field>
     <v-btn @click="printMarkDown">
       마크 다운 출력
+    </v-btn>
+    <v-btn @click="sendFlutterWebviewMessage">
+      Flutter webview로 통신
     </v-btn>
     <v-btn @click="printHtml">
       Html 출력
@@ -32,7 +38,8 @@ export default Vue.extend({
   },
   data(){
     return {
-      editor: {} as EditorCore
+      editor: {} as EditorCore,
+      testData: ""
     }
   },
   mounted() {
@@ -59,13 +66,26 @@ export default Vue.extend({
         }
       }
     })
+    let item = window.localStorage.getItem("mainText");
+    if(item){
+      this.testData = item;
+    }
+
   },
   methods:{
     printMarkDown(){
       console.log(this.editor.getMarkdown())
     },
+    sendFlutterWebviewMessage(){
+      let win = window as any;
+      win.wecango.postMessage('Hello from JS');
+      console.log("userAgent =" +navigator.userAgent)
+    },
     printHtml(){
       console.log(this.editor.getHTML())
+    },
+    onchange(value: string){
+      window.localStorage.setItem("mainText",value)
     }
   }
 
